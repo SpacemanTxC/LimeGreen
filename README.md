43b6097c1560f3699b05c01af9102282e888569d
Tate Capra <tatecapra@outlook.com>
2023-01-22 19:20:14 -0500

    Adding React

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LimeGreen
 Working website for an upcoming business. 
+Hello! My name is Tate.
+
+Inprogress App: Web App for an upcoming business.
+
+Contact Info: Profile Page: https://spacemantxc.github.io/Profile/ Linkedin: www.linkedin.com/in/tate-capra
---
+++ lime-green/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import logo from './Logo.png'; // Add logo image
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          LimeGreen Coming Soon
+        </p>
+        <img src ={logo} alt ='logo' />
+        <h1>
+          Spring 2023
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
